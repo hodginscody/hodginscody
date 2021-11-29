b5e1afc6a122fbee8737c69e9ac98ce0979333d2
hodginscody <93688997+hodginscody@users.noreply.github.com>
2021-11-29 01:40:00 -0500

    HW4 commit

--- CS601_HW4_hodgins/hodginsJavascript.js
+++ CS601_HW4_hodgins/hodginsJavascript.js
@@ -2,23 +2,60 @@
 //     let clickedSubmit = document.getElementById('submit');
 //     clickedSubmit.onclick = Submit;
 //  }
+
+// Create an array of active facilitators in this class that we 
+// will validate input against
+let validFacilitators = ["Josh", "Chris", "Fazil", "Christian"];
    
 function Submit() {
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
     let facilitator = document.getElementById("facilitator").value;
+    // Create an array that contains firstName and lastName, so we don't
+    // have extra code and can just loop through each validation
+    let nameArray = [firstName, lastName];
+    // passedValidation either equals false (which means we don't complete 
+    // submitting the form) or the web address we are submitting to
     let passedValidation;
-    if (firstName.length < 2) {
-        alert("First Name must be at least two characters in the alphabet.");
-        passedValidation = false;
-    }
-    for (i in firstName) {
-        if (!(firstName[i] in [A-Z] || firstName[i] in [a-z])) {
-            alert("First Name can only contain characters in the alphabet.");
+    // Loop through nameArray to validate firstName and lastName
+    for (var j = 0; j < nameArray.length; j++) {
+        if (nameArray[j].length < 2) {
+            alert("First Name must be at least two characters in the alphabet.");
+            // Will not return here because I want to let the user know if they've
+            // entered invalid characters as well.
             passedValidation = false;
         }
+        // Loop through each character in firstName or lastName and verify it is
+        // in the alphabet
+        for (var i = 0; i < nameArray[j].length; i++) {
+            // charAt() returns the character at position i
+            // match() determines if the character is within A-Z
+            // i makes the Regex case insensitive
+            if (nameArray[j].charAt(i).match(/[A-Z]/i) == null) {
+                alert("First Name can only contain characters in the alphabet.");
+                passedValidation = false;
+                // Return to the form. No need to check the rest of the characters
+                return passedValidation;
+            }
+        }
     }
-    if (!(passedValidation == false)) {
+    // Next, validate that the facilitator the user entered is valid
+    for (var k = 0; k < validFacilitators.length; k++) {
+        // If we find a matching facilitator, then break from this loop
+        if (facilitator == validFacilitators[k]) {
+            break;
+        }
+    }
+    // If facilitator was not found in validFacilitators, then return an error
+    // If k equals the length of validFacilitators, then we know the above for loop
+    // did not find any match.
+    if (k == validFacilitators.length) {
+        alert("You entered an invalid facilitator. Please enter one of the following: " + validFacilitators.join(","));
+        passedValidation = false;
+        return passedValidation;
+    }
+    // If validation passes, then return the web address of the database we are submitting to  
+    if (passedValidation !== false) {
         passedValidation = "https://bucs601.com/submit.php";
     }
     return passedValidation;
