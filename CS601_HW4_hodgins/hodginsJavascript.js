880d897b25f1528810fdbfd4c494782e73ba3df5
hodginscody <93688997+hodginscody@users.noreply.github.com>
2021-11-29 21:45:21 -0500

    HW4 commit

--- CS601_HW4_hodgins/hodginsJavascript.js
+++ CS601_HW4_hodgins/hodginsJavascript.js
@@ -23,10 +23,11 @@ function Submit() {
             // j = 0 -> First Name
             // j = 1 -> Last Name
             if (j == 0) {
-                alert("First Name must be at least two characters in the alphabet.");
+                document.getElementById('errors').innerHTML="**First Name must be at least two characters in the alphabet.**";
+                // alert("First Name must be at least two characters in the alphabet.");
                 document.getElementById("firstName").value = "";
             } else {
-                alert("Last Name must be at least two characters in the alphabet.");
+                // alert("Last Name must be at least two characters in the alphabet.");
                 document.getElementById("lastName").value = "";
             }
             // Will not return here because I want to let the user know if they've
@@ -41,11 +42,11 @@ function Submit() {
             // i makes the Regex case insensitive
             if (nameArray[j].charAt(i).match(/[A-Z]/i) == null) {
                 if (j == 0) {
-                    alert("First Name can only contain characters in the alphabet.");
+                    // alert("First Name can only contain characters in the alphabet.");
                     // If user entry is invalid, set the field to blank
                     document.getElementById("firstName").value = "";
                 } else {
-                    alert("Last Name can only contain characters in the alphabet.");
+                    // alert("Last Name can only contain characters in the alphabet.");
                     document.getElementById("lastName").value = "";
                 }
                 passedValidation = false;
@@ -66,7 +67,7 @@ function Submit() {
     // If k equals the length of validFacilitators, then we know the above for loop
     // did not find any match.
     if (k == validFacilitators.length) {
-        alert("You entered an invalid facilitator. Please enter one of the following: " + validFacilitators.join(","));
+        // alert("You entered an invalid facilitator. Please enter one of the following: " + validFacilitators.join(","));
         document.getElementById("facilitator").value = "";
         passedValidation = false;
         return passedValidation;
