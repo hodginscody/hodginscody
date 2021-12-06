24223b58da955abcba403a281f801998dc80c6b4
hodginscody <93688997+hodginscody@users.noreply.github.com>
2021-12-06 02:29:18 -0500

    HW5 commit

--- CS601_HW5_hodgins/hodginsGetJSON.js
+++ CS601_HW5_hodgins/hodginsGetJSON.js
@@ -1,13 +1,33 @@
-async function getCollegeDegrees(url = '') {
-    await fetch(url).then((response) => response.json())
-      .then((data) => {
-          if (response.status !== "200") {
-            alert("Error with request");
-          } else {
-          document.write("In ${data.data[0].bachelors.yearConferred}, I received a ${data.data[0].bachelors.type} \
-            in ${data.data[0].bachelors.program} from ${data.data[0].bachelors.school}");
-          }
-      )
+async function getCollegeDegrees(url = '/CS601_HW5_hodgins/my_college_degrees.json') {
+    await fetch(url, {
+      method: "GET",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+    .then(response => { 
+      // response.ok checks whether status is within 200s, which is good, or anything else, which is good
+      if (!response.ok) {
+        // notify user of the response status code
+        throw new Error("HTTP error " + response.status);
+      }
+      return response.json(); })
+    .then(data => {
+        for (var i=0; i < data.my_college_degrees.length; i++) {
+          let school = data.my_college_degrees[i].degree.school;
+          let program = data.my_college_degrees[i].degree.program;
+          let type = data.my_college_degrees[i].degree.type;
+          let year = data.my_college_degrees[i].degree.yearConferred;
+          // Write data to our html page
+          document.write(`In ${year}, I received a ${type} in ${program} from ${school}`);
+        }
+    })
+    .catch(function() {
+      // element.parentElement.innerHTML = `Error: ${error}`;
+      console.error('There was an error!');
+    });
       
 }
   
